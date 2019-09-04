8e9b712fda07fc06163a69fb71432c8ab1fd5611
Tianyi Liu <liuty10@gmail.com>
2019-09-04 16:31:35 -0500

    train file

---
+++ mobileNet-4-1-runTrain_imhotepvr.sh
@@ -0,0 +1,4 @@
+python3 object_detection/train.py \
+--logtostderr \
+--pipeline_config_path=/media/lty/newspace/BenchmarkFrameWork/data-sets/imhotepvr/ssd_mobilenet_v1_imhotepvr.config \
+--train_dir=/media/lty/newspace/BenchmarkFrameWork/data-sets/imhotepvr/training-result/
