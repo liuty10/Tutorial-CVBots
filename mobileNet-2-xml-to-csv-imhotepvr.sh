fda372b3880d053d1c511ba368987067fe7b1d04
Tianyi Liu <liuty10@gmail.com>
2019-09-04 14:23:53 -0500

    xml to csv.sh

---
+++ mobileNet-2-xml-to-csv-imhotepvr.sh
@@ -0,0 +1,4 @@
+#training
+python ./mobileNet-2-xml-to-csv.py /media/lty/newspace/BenchmarkFrameWork/data-sets/imhotepvr/train
+#testing
+#python ./mobileNet-2-xml-to-csv.py /media/lty/newspace/BenchmarkFrameWork/data-sets/imhotepvr/testing
