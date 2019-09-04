f86ca2605d63ba308c9ce42a8f66d4550c79a382
Tianyi Liu <liuty10@gmail.com>
2019-09-04 14:28:07 -0500

    create record file

---
+++ mobileNet-3-generate_tfrecord_imhotepvr.sh
@@ -0,0 +1,5 @@
+#training
+python mobileNet-3-generate_tfrecord_imhotepvr.py --image_path=/media/lty/newspace/BenchmarkFrameWork/data-sets/imhotepvr/train --csv_input=/media/lty/newspace/BenchmarkFrameWork/data-sets/imhotepvr/train/tf.csv --output_path=/media/lty/newspace/BenchmarkFrameWork/data-sets/imhotepvr/train/train.record
+
+#testing
+#python mobileNet-3-generate_tfrecord_0ad.py --image_path=/home/tianyiliu/Documents/workspace/gaming/myprojects/renderBench/bench-datasets/0ad/0ad-test --csv_input=/home/tianyiliu/Documents/workspace/gaming/myprojects/renderBench/bench-datasets/0ad/0ad-test/tf.csv --output_path=/home/tianyiliu/Documents/workspace/gaming/myprojects/renderBench/bench-datasets/0ad/0ad-test/test.record
