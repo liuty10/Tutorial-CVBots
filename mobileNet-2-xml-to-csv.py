6ab4bf7b6b23371be2260f32f0eee3c30c73c162
tinayi liu <liuty10@gmail.com>
2019-09-04 14:06:34 -0500

    xml to csv .py

---
+++ mobileNet-2-xml-to-csv.py
@@ -0,0 +1,42 @@
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+import sys
+
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("please specify directory to xml files")
+        print("Format: python ./xml_to_csv.py <DIR>")
+        sys.exit()
+
+    xml_path 	= sys.argv[1]
+    xml_df 	= xml_to_csv(xml_path)
+    csv_path 	= os.path.join(xml_path,'tf.csv')
+    xml_df.to_csv(csv_path, index=None)
+    print('Successfully converted xml to csv.')
+    print("Here is the csv file: ", csv_path)
+
+
+main()
